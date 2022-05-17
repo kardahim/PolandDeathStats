e4426c0e76a2dab3d982d50431740aed641d818f
Paweł Ciszewski <kardahim@gmail.com>
2022-05-17 16:51:41 +0200

    add BrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
 import './App.scss';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+// import pages
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
+import Home from './pages/home/Home'
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
