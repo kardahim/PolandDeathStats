9985426dcf3af6feca7066415fef7b8a54b65ade
Paweł Ciszewski <kardahim@gmail.com>
2022-05-19 18:51:23 +0200

    add registerForm

---
+++ client/src/containers/registerForm/RegisterForm.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import './registerForm.scss'
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
+import TextField from '../../components/textField/TextField'
+import Button from '../../components/button/Button'
+
+function RegisterForm() {
+    const initialValues = {
+        firstname: '',
+        lastname: '',
+        email: '',
+        password: ''
+    }
+
+    // TODO: add validation
+    const validationSchema = Yup.object().shape({
+        firstname: Yup.string()
+            .required('Pole jest wymagane'),
+        lastname: Yup.string()
+            .required('Pole jest wymagane'),
+        email: Yup.string()
+            .email("To nie jest email")
+            .required('Pole jest wymagane'),
+        password: Yup.string()
+            .required('Pole jest wymagane')
+    })
+
+    const onSubmit = (data) => {
+        // TODO: submit to server
+        console.log("register submit button pressed")
+    }
+
+    return (
+        <div className='register-container'>
+            <Formik
+                initialValues={initialValues}
+                onSubmit={onSubmit}
+                validationSchema={validationSchema}>
+                <Form className='register-form'>
+                    <section className='header'>
+                        register
+                    </section>
+                    {/* firstname */}
+                    <TextField
+                        name='firstname'
+                        className='register-input'
+                        placeholder='Imię'
+                    />
+                    {/* lastname */}
+                    <TextField
+                        name='lastname'
+                        className='register-input'
+                        placeholder='Nazwisko'
+                    />
+                    {/* email */}
+                    <TextField
+                        autoComplete='username'
+                        name='email'
+                        className='register-input'
+                        placeholder='Email'
+                    />
+                    {/* password */}
+                    <TextField
+                        autoComplete='current-password'
+                        name='password'
+                        className='register-input'
+                        placeholder='Hasło'
+                        type='password' />
+                    {/* submit button */}
+                    <Button
+                        type='submit'
+                        text='Zarejestruj się' />
+                </Form>
+            </Formik>
+        </div>
+    )
+}
+
+export default RegisterForm
