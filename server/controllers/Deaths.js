a9653915720bf526eb14527f708c8db044d0e804
damianboguszpl <damian.bogusz@pollub.edu.pl>
2022-05-23 12:48:43 +0200

    ?

---
+++ server/controllers/Deaths.js
@@ -0,0 +1,36 @@
+const { Death } = require("../db/models")
+
+const getDeaths = ((req, res) => {
+
+})
+
+const getDeathById = ((req, res) => {
+
+})
+
+const getDeathByYear = ((req, res) => {
+
+})
+
+const getDeathByRegionId = ((req, res) => {
+
+})
+
+const getDeathByDeathCauseId = ((req, res) => {
+
+})
+
+const getDeathByYearRegionIdAndDeathCauseId = ((req, res) => {
+
+})
+
+
+
+module.exports = {
+    getDeaths,
+    getDeathById,
+    getDeathByYear,
+    getDeathByRegionId,
+    getDeathByDeathCauseId,
+    getDeathByYearRegionIdAndDeathCauseId
+}
--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = 3001
 const db = require('./db/models')
+app.use(express.json());
+app.use(cors());
 
 //Routers
 const usersRouter = require('./routes/Users');
+app.use("/users", usersRouter);
 const rolesRouter = require('./routes/Roles');
+app.use("/roles", rolesRouter);
 const regionsRouter = require('./routes/Regions');
+app.use("/regions", regionsRouter);
 const populationsRouter = require('./routes/Populations');
+app.use("/deaths", populationsRouter);
 const deathsRouter = require('./routes/Deaths');
+app.use("/deaths", deathsRouter);
 const deathCausesRouter = require('./routes/DeathCauses');
+app.use("/deathcauses", deathCausesRouter);
 
 db.sequelize.sync().then(() => {
     app.listen(port, () => console.log(`Server listening on port ${port}!`))
