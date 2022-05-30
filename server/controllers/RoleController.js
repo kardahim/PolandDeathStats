15cd71231de2d3135c104a139730efe1f818a39b
damianboguszpl <damian.bogusz@pollub.edu.pl>
2022-05-30 20:44:52 +0200

    ADDED: adding User_Roles & adding Roles

--- server/controllers/RoleController.js
+++ server/controllers/RoleController.js
@@ -19,5 +19,23 @@ module.exports = {
         const name = req.params.name
         const role = await Role.findOne({ where: { name: name } });
         res.json(role);
+    },
+
+    // add Role
+    addRole: async (req, res) => {
+        const { name } = req.body;
+
+        const role = await Role.findOne({ where: { name: name } });
+        if (role) {
+            res.json({ error: "A role with given name already exists." });
+        }
+        else {
+            Role.create({
+                name:name
+            })
+            
+            res.json("Role added successfully.");
+        }
     }
+
 }
